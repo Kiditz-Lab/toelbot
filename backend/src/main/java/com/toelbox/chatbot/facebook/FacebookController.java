d5b2d7a055afd7eaa2775d73f3c5170616a321fe
rifkyaditya <kiditzbastara@gmail.com>
2025-04-15 05:59:10 +0700

    remove public

--- backend/src/main/java/com/toelbox/chatbot/facebook/FacebookController.java
+++ backend/src/main/java/com/toelbox/chatbot/facebook/FacebookController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/facebook")
 @RequiredArgsConstructor
-public class FacebookController {
+class FacebookController {
 
 	private final FacebookService facebookService;
 
