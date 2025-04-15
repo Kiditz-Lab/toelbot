5edf81cd4f64a56deafaf04c3463338e22e8aae5
rifkyaditya <kiditzbastara@gmail.com>
2025-04-16 06:11:28 +0700

    config instagram

---
+++ frontend/src/stores/apps/instagramStore.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+
+export const useInstagramStore = defineStore('instagramStore', () => {
+  const connectInstagram = () => {
+    const clientId = import.meta.env.VITE_FACEBOOK_APP_ID;
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+
+    const scope = 'pages_show_list,instagram_basic,instagram_manage_messages';
+    const redirectUri = `${baseUrl}/instagram/callback`;
+
+    const igAuthUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+      redirectUri
+    )}&scope=${scope}&response_type=code`;
+
+    window.open(igAuthUrl, '_blank', 'width=500,height=600');
+  };
+
+  return { connectInstagram };
+});
---
+++ frontend/src/views/apps/integration/InstagramCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useInstagramStore } from '@/stores/apps/instagramStore';
+const store = useInstagramStore();
+</script>
+<template>
+  <v-card title="Instagram" subtitle="Integrate Toelbot to Instagram">
+    <template v-slot:prepend>
+      <v-avatar rounded>
+        <v-img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" contain />
+      </v-avatar>
+    </template>
+    <v-card-text> Connect your Toelbot to your Instagram <b>Creator/Business</b> Account to chat with your customer 24/7.</v-card-text>
+    <v-card-actions>
+      <v-btn color="pink" block variant="elevated" @click="store.connectInstagram">Connect</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
