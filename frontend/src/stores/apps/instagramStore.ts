b6bf6d5e95628588c0747d3c34b01e1281a58ff8
rifkyaditya <kiditzbastara@gmail.com>
2025-04-16 21:43:42 +0700

    return account to ui

--- frontend/src/stores/apps/instagramStore.ts
+++ frontend/src/stores/apps/instagramStore.ts
@@ -5,13 +5,13 @@ export const useInstagramStore = defineStore('instagramStore', () => {
     const clientId = import.meta.env.VITE_INSTAGRAM_APP_ID; // Same as Facebook
     const baseUrl = import.meta.env.VITE_BASE_URL;
 
-    const scope = 'instagram_basic,instagram_manage_messages,instagram_manage_comments,instagram_content_publish,instagram_manage_insights';
+    const scope = 'instagram_business_basic,instagram_business_manage_messages';
     const redirectUri = `${baseUrl}/instagram/callback`;
 
     const igAuthUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${clientId}&redirect_uri=${encodeURIComponent(
       redirectUri
     )}&scope=${scope}&response_type=code`;
-
+    console.log(igAuthUrl);
     window.open(igAuthUrl, '_blank', 'width=500,height=600');
   };
 
