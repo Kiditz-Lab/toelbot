3ee24214c4593b4079dd14714b4f7fc5f53128b5
rifkyaditya <kiditzbastara@gmail.com>
2025-04-17 02:17:04 +0700

    fix migration

--- backend/src/main/resources/db/migration/V15__create_table_instagram_account.sql
+++ backend/src/main/resources/db/migration/V15__create_table_instagram_account.sql
@@ -1,6 +1,7 @@
 CREATE TABLE instagram_account (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id TEXT NOT NULL,
+    agent_id UUID,
     profile_picture_url TEXT,
     name TEXT,
     username TEXT,
